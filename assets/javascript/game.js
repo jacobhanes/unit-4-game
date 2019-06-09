a650864e1e92a2c8bfa845e3a25ff8b85edd47e3
jake hanes <jhanesx@yahoo.com>
2019-06-08 23:38:30 -0700

    images have random values now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,10 @@ let losses = 0;
 let myScore = 0;
 let goal = Math.floor(Math.random()*120)+19;
 let gameStarted = false;
-let worth = Math.floor(Math.random()*13);
+let worthOne = Math.floor(Math.random()*13);
+let worthTwo = Math.floor(Math.random()*13);
+let worthThree = Math.floor(Math.random()*13);
+let worthFour = Math.floor(Math.random()*13);
 
 function initializeGame(){
     goal = Math.floor(Math.random()*120)+19;
@@ -14,23 +17,31 @@ function initializeGame(){
 }
 
 $("#gemOne").on("click", function(){
-    console.log("clicked");
-
+    
+$("#gemOne").attr("crystalValue", worthOne);
+const gemValue = ($(this).attr("crystalValue"));
+console.log(gemValue);
 });
 
 $("#gemTwo").on("click", function(){
-    console.log("clicked");
-
-});
+    
+    $("#gemTwo").attr("crystalValue", worthTwo);
+    const gemValue = ($(this).attr("crystalValue"));
+    console.log(gemValue);
+    });
 
 $("#gemThree").on("click", function(){
     console.log("clicked");
-
+    $("#gemThree").attr("crystalValue", worthThree);
+    const gemValue = ($(this).attr("crystalValue"));
+    console.log(gemValue);
 });
 
 $("#gemFour").on("click", function(){
     console.log("clicked");
-
+    $("#gemFour").attr("crystalValue", worthFour);
+    const gemValue = ($(this).attr("crystalValue"));
+    console.log(gemValue);
 });
 
 
