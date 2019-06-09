d99f50e433033c17f5579d97e8bda6e0306eff83
jake hanes <jhanesx@yahoo.com>
2019-06-08 23:47:57 -0700

    on click adds value to counter in log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-let wins = 0;
-let losses = 0;
-let myScore = 0;
-let goal = Math.floor(Math.random()*120)+19;
-let gameStarted = false;
-let worthOne = Math.floor(Math.random()*13);
-let worthTwo = Math.floor(Math.random()*13);
-let worthThree = Math.floor(Math.random()*13);
-let worthFour = Math.floor(Math.random()*13);
+    let wins = 0;
+    let losses = 0;
+    let myScore = 0;
+    let goal = Math.floor(Math.random() * 120) + 19;
+    let gameStarted = false;
+    let worthOne = Math.floor(Math.random() * 13)+1;
+    let worthTwo = Math.floor(Math.random() * 13)+1;
+    let worthThree = Math.floor(Math.random() * 13)+1;
+    let worthFour = Math.floor(Math.random() * 13)+1;
 
-function initializeGame(){
-    goal = Math.floor(Math.random()*120)+19;
-    myScore = 0;
-    gameStarted = false;
-}
+    function initializeGame() {
+        goal = Math.floor(Math.random() * 120) + 19;
+        myScore = 0;
+        gameStarted = false;
+    }
 
-$("#gemOne").on("click", function(){
-    
-$("#gemOne").attr("crystalValue", worthOne);
-const gemValue = ($(this).attr("crystalValue"));
-console.log(gemValue);
-});
+    $("#gemOne").on("click", function () {
 
-$("#gemTwo").on("click", function(){
-    
-    $("#gemTwo").attr("crystalValue", worthTwo);
-    const gemValue = ($(this).attr("crystalValue"));
-    console.log(gemValue);
+        $("#gemOne").attr("crystalValue", worthOne);
+        let gemValue = ($(this).attr("crystalValue"));
+        console.log(gemValue);
+        gemValue = parseInt(gemValue);
+        myScore += gemValue;
+        console.log(myScore);
     });
 
-$("#gemThree").on("click", function(){
-    console.log("clicked");
-    $("#gemThree").attr("crystalValue", worthThree);
-    const gemValue = ($(this).attr("crystalValue"));
-    console.log(gemValue);
-});
+    $("#gemTwo").on("click", function () {
 
-$("#gemFour").on("click", function(){
-    console.log("clicked");
-    $("#gemFour").attr("crystalValue", worthFour);
-    const gemValue = ($(this).attr("crystalValue"));
-    console.log(gemValue);
-});
+        $("#gemTwo").attr("crystalValue", worthTwo);
+        let gemValue = ($(this).attr("crystalValue"));
+        console.log(gemValue);
+        gemValue = parseInt(gemValue);
+        myScore += gemValue;
+        console.log(myScore);
+    });
+
+    $("#gemThree").on("click", function () {
+
+        $("#gemThree").attr("crystalValue", worthThree);
+        let gemValue = ($(this).attr("crystalValue"));
+        console.log(gemValue);
+        gemValue = parseInt(gemValue);
+        myScore += gemValue;
+        console.log(myScore);
+    });
+
+    $("#gemFour").on("click", function () {
+
+        $("#gemFour").attr("crystalValue", worthFour);
+        let gemValue = ($(this).attr("crystalValue"));
+        console.log(gemValue);
+        gemValue = parseInt(gemValue);
+        myScore += gemValue;
+        console.log(myScore);
+    });
 
 
 
