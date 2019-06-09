3789abe17868187d867fee852bc47a4205d250ec
jake hanes <jhanesx@yahoo.com>
2019-06-08 20:33:03 -0700

    img have a click and js linked

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+let wins = 0;
+let losses = 0;
+let myScore = 0;
+let goal = Math.floor(Math.random()*13);
+
+$(".crystal").on("click", function(){
+    console.log("clicked");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
