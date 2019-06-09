c089c64637cd64d0ed19cc7763c636e5a83a1f40
jake hanes <jhanesx@yahoo.com>
2019-06-08 20:42:11 -0700

    initialize game function made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,17 @@ let wins = 0;
 let losses = 0;
 let myScore = 0;
 let goal = Math.floor(Math.random()*13);
+let gameStarted = false;
+
+function initializeGame(){
+    goal = Math.floor(Math.random()*13);
+    myScore = 0;
+    gameStarted = false;
+}
 
 $(".crystal").on("click", function(){
     console.log("clicked");
+    
 })
 
 
