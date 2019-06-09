eef99fea5925b39b9a5b7def6359db208229b84a
jake hanes <jhanesx@yahoo.com>
2019-06-08 23:07:59 -0700

    changed 1 onclick to 4 seperate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,25 +3,35 @@ $(document).ready(function(){
 let wins = 0;
 let losses = 0;
 let myScore = 0;
-let goal = Math.floor(Math.random()*13);
+let goal = Math.floor(Math.random()*120)+19;
 let gameStarted = false;
+let worth = Math.floor(Math.random()*13);
 
 function initializeGame(){
-    goal = Math.floor(Math.random()*13);
+    goal = Math.floor(Math.random()*120)+19;
     myScore = 0;
     gameStarted = false;
 }
 
-$(".crystal").on("click", function(){
+$("#gemOne").on("click", function(){
     console.log("clicked");
-    
-})
 
+});
 
+$("#gemTwo").on("click", function(){
+    console.log("clicked");
 
+});
 
+$("#gemThree").on("click", function(){
+    console.log("clicked");
 
+});
 
+$("#gemFour").on("click", function(){
+    console.log("clicked");
+
+});
 
 
 
